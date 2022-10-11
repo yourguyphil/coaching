e44e257c3cb85105581e172d01da8395cb41ea0f
Philips Nguyen <philips.nguyen96@gmail.com>
2022-10-11 12:03:37 -0400

    Fix meta image

--- nextjs/src/pages/_document.tsx
+++ nextjs/src/pages/_document.tsx
@@ -53,7 +53,7 @@ I offer to you my master class TCG coaching!"
           ></meta>
           <meta
             property="og:image"
-            content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+            content="https://philipsweb.blob.core.windows.net/$web/metatagimage.jpg"
           ></meta>
 
           <meta property="twitter:card" content="summary_large_image"></meta>
@@ -79,7 +79,7 @@ I offer to you my master class TCG coaching!"
           ></meta>
           <meta
             property="twitter:image"
-            content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+            content="https://philipsweb.blob.core.windows.net/$web/metatagimage.jpg"
           ></meta>
         </Head>
         <body>
