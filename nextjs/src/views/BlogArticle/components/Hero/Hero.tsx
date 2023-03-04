7105a3f740770bc7e3699520bb236fcd1bfe009a
Philips Nguyen <philips.nguyen96@gmail.com>
2023-03-04 00:45:16 -0500

    Modals for articles

--- nextjs/src/views/BlogArticle/components/Hero/Hero.tsx
+++ nextjs/src/views/BlogArticle/components/Hero/Hero.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 import React, { useEffect } from 'react';
 import { alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
--- nextjs/src/views/BlogArticle/components/SimilarStories/SimilarStories.tsx
+++ nextjs/src/views/BlogArticle/components/SimilarStories/SimilarStories.tsx
@@ -12,6 +12,9 @@ import Grid from '@mui/material/Grid';
 import Avatar from '@mui/material/Avatar';
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import Modal from '@mui/material/Modal';
+import { IconButton, Stack, TextField } from '@mui/material';
+import Close from '@mui/icons-material/Close';
+import Image from 'mui-image';
 
 export type PromotionInputs = {
   locationOfPromotion: string;
@@ -36,11 +39,22 @@ export interface Article {
   description: string;
 }
 
+const style = {
+  backgroundColor: 'white',
+  width: '95%',
+  height: '95%',
+  outline: 'black',
+};
+
 const SimilarStories = (): JSX.Element => {
   const theme = useTheme();
   const cmsArticleURL = 'https://cms.philipsnguyen.com/items/articles';
 
   const [articles, setArticles] = useState<Articles>(null);
+  const [selectedArticle, setSelectedArticle] = useState(null);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   useEffect(() => {
     axios
@@ -58,86 +72,178 @@ const SimilarStories = (): JSX.Element => {
   return (
     <Box>
       <Grid container spacing={4}>
-        {articles?.data.map((item, i) => (
-          <Grid item xs={12} md={4} key={i}>
-            <Box
-              component={'div'}
-              display={'block'}
-              width={1}
-              height={1}
+        {articles?.data.map((article, i) => (
+          <>
+            <Grid item xs={12} md={4} key={i}>
+              <Box
+                component={'div'}
+                display={'block'}
+                width={1}
+                height={1}
+                sx={{
+                  textDecoration: 'none',
+                  transition: 'all .2s ease-in-out',
+                  '&:hover': {
+                    transform: `translateY(-${theme.spacing(1 / 2)})`,
+                    cursor: 'pointer',
+                  },
+                }}
+                onClick={() => setSelectedArticle(article)}
+              >
+                <Card
+                  onClick={handleOpen}
+                  component={Box}
+                  width={1}
+                  height={1}
+                  boxShadow={4}
+                  display={'flex'}
+                  flexDirection={'column'}
+                  sx={{ backgroundImage: 'none' }}
+                >
+                  <CardMedia
+                    image={article.main_image}
+                    title={article.title}
+                    sx={{
+                      height: { xs: 300, md: 360 },
+                      position: 'relative',
+                    }}
+                  >
+                    <Box
+                      component={'svg'}
+                      viewBox="0 0 2880 480"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      sx={{
+                        position: 'absolute',
+                        bottom: 0,
+                        color: theme.palette.background.paper,
+                        transform: 'scale(2)',
+                        height: 'auto',
+                        width: 1,
+                        transformOrigin: 'top center',
+                      }}
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M2160 0C1440 240 720 240 720 240H0v240h2880V0h-720z"
+                        fill="currentColor"
+                      />
+                    </Box>
+                  </CardMedia>
+                  <Box component={CardContent} position={'relative'}>
+                    <Typography variant={'h6'} gutterBottom>
+                      {article.title}
+                    </Typography>
+                    <Typography color="text.secondary">
+                      {article.description}
+                    </Typography>
+                  </Box>
+                  <Box flexGrow={1} />
+                  <Box padding={2} display={'flex'} flexDirection={'column'}>
+                    <Box marginBottom={2}>
+                      <Divider />
+                    </Box>
+                    <Box
+                      display={'flex'}
+                      justifyContent={'space-between'}
+                      alignItems={'center'}
+                    ></Box>
+                  </Box>
+                </Card>
+              </Box>
+            </Grid>
+          </>
+        ))}
+      </Grid>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby={selectedArticle?.title}
+        aria-describedby={selectedArticle?.description}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Box
+          sx={{
+            position: 'relative',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'stretch',
+            maxWidth: '80%',
+            maxHeight: '80%',
+            backgroundColor: '#222b45',
+            borderRadius: '8px',
+            padding: '16px',
+            boxShadow: '0px 4px 16px rgba(0, 0, 0, 0.1)',
+            overflow: 'auto', // add overflow property here
+          }}
+        >
+          <IconButton
+            sx={{
+              position: 'absolute',
+              top: '8px',
+              right: '8px',
+              color: 'white',
+            }}
+            onClick={handleClose}
+          >
+            <Close />
+          </IconButton>
+          <Box sx={{ paddingBottom: 3 }}>
+            <Typography
+              id={selectedArticle?.title}
+              variant="h3"
+              sx={{ color: 'white', textAlign: 'center' }}
+            >
+              {selectedArticle?.title}
+            </Typography>
+          </Box>
+          <Image
+            src={selectedArticle?.main_image}
+            style={{
+              width: '25%',
+              height: '25%',
+              paddingBottom: 10,
+              //Stretch to fit width
+              objectFit: 'cover',
+              border: '5px solid white',
+            }}
+          />
+          <Typography
+            id="modal-modal-description"
+            sx={{ mt: 2, color: 'white' }}
+            dangerouslySetInnerHTML={{ __html: selectedArticle?.article }}
+          />
+          <IconButton
+            sx={{
+              backgroundColor: '#377dff',
+              borderRadius: '8px',
+              padding: '8px',
+              '&:hover': {
+                backgroundColor: '#2f6ad9',
+              },
+            }}
+            onClick={handleClose}
+          >
+            <Typography
+              variant="button"
+              component="span"
               sx={{
-                textDecoration: 'none',
-                transition: 'all .2s ease-in-out',
+                color: 'white',
                 '&:hover': {
-                  transform: `translateY(-${theme.spacing(1 / 2)})`,
-                  cursor: 'pointer',
+                  backgroundColor: 'white',
                 },
               }}
             >
-              <Box
-                component={Card}
-                width={1}
-                height={1}
-                boxShadow={4}
-                display={'flex'}
-                flexDirection={'column'}
-                sx={{ backgroundImage: 'none' }}
-              >
-                <CardMedia
-                  image={item.main_image}
-                  title={item.title}
-                  sx={{
-                    height: { xs: 300, md: 360 },
-                    position: 'relative',
-                  }}
-                >
-                  <Box
-                    component={'svg'}
-                    viewBox="0 0 2880 480"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    sx={{
-                      position: 'absolute',
-                      bottom: 0,
-                      color: theme.palette.background.paper,
-                      transform: 'scale(2)',
-                      height: 'auto',
-                      width: 1,
-                      transformOrigin: 'top center',
-                    }}
-                  >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M2160 0C1440 240 720 240 720 240H0v240h2880V0h-720z"
-                      fill="currentColor"
-                    />
-                  </Box>
-                </CardMedia>
-                <Box component={CardContent} position={'relative'}>
-                  <Typography variant={'h6'} gutterBottom>
-                    {item.title}
-                  </Typography>
-                  <Typography color="text.secondary">
-                    {item.description}
-                  </Typography>
-                </Box>
-                <Box flexGrow={1} />
-                <Box padding={2} display={'flex'} flexDirection={'column'}>
-                  <Box marginBottom={2}>
-                    <Divider />
-                  </Box>
-                  <Box
-                    display={'flex'}
-                    justifyContent={'space-between'}
-                    alignItems={'center'}
-                  ></Box>
-                </Box>
-              </Box>
-            </Box>
-          </Grid>
-        ))}
-      </Grid>
+              Close
+            </Typography>
+          </IconButton>
+        </Box>
+      </Modal>
     </Box>
   );
 };
