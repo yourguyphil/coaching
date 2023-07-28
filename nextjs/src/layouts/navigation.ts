f839ad704ad0e0bda98d9012e74492c19eb5986c
Philips Nguyen <philips.nguyen96@gmail.com>
2023-07-27 22:11:14 -0400

    Affiliate Link

--- nextjs/src/layouts/navigation.ts
+++ nextjs/src/layouts/navigation.ts
@@ -11,6 +11,10 @@ const pages = {
     {
       title: 'Magic The Gathering Meta',
       href: 'https://www.mtggoldfish.com/metagame/pioneer#paper',
+    },
+    {
+      title: 'Link to Cheap Bulk Sleeves',
+      href: 'https://www.amazon.com/Dragon-Shield-Standard-Sleeves-Display/dp/B07KY84N39/ref=sr_1_8?keywords=dragon+shield+sleeves+white&amp;qid=1690509886&amp;sprefix=dragon+shield+sleeves+white%252Caps%252C119&amp;sr=8-8&amp;ufe=app_do%253Aamzn1.fos.18ed3cb5-28d5-4975-8bc7-93deae8f9840&_encoding=UTF8&tag=officegadge03-20&linkCode=ur2&linkId=cb7e5570c5b4a6bb7f7b149136dcb3b5&camp=1789&creative=9325',
     }
   ],
   freecoaching: [
