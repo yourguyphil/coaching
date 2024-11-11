8daef0f91be5861e1ced71dbe6bd8465d80acc4e
Philips Nguyen <philips.nguyen96@gmail.com>
2024-11-11 03:42:56 -0500

    affil links

--- nextjs/src/layouts/navigation.ts
+++ nextjs/src/layouts/navigation.ts
@@ -1,6 +1,18 @@
 const pages = {
   portfolio: [
     {
+      title: 'Magic The Gathering Meta',
+      href: 'https://www.mtggoldfish.com/metagame/pioneer#paper',
+    },
+    {
+      title: 'Pokemon Pocket Meta',
+      href: 'https://play.limitlesstcg.com/tournaments/completed',
+    },
+    {
+      title: 'Lorcana Meta',
+      href: 'https://dreamborn.ink/events',
+    },
+    {
       title: 'Gods Unchained Referral',
       href: '/referral',
     },
@@ -9,12 +21,16 @@ const pages = {
       href: 'https://gudecks.com/meta/tier-list',
     },
     {
-      title: 'Magic The Gathering Meta',
-      href: 'https://www.mtggoldfish.com/metagame/pioneer#paper',
+      title: 'Link to Cheap Bulk Sleeves',
+      href: 'https://amzn.to/3YGW0rI',
     },
     {
-      title: 'Link to Cheap Bulk Sleeves',
-      href: 'https://www.amazon.com/Dragon-Shield-Standard-Sleeves-Display/dp/B07KY84N39/ref=sr_1_8?keywords=dragon+shield+sleeves+white&amp;qid=1690509886&amp;sprefix=dragon+shield+sleeves+white%252Caps%252C119&amp;sr=8-8&amp;ufe=app_do%253Aamzn1.fos.18ed3cb5-28d5-4975-8bc7-93deae8f9840&_encoding=UTF8&tag=officegadge03-20&linkCode=ur2&linkId=cb7e5570c5b4a6bb7f7b149136dcb3b5&camp=1789&creative=9325',
+      title: 'Link to Cheap Battery Pack',
+      href: 'https://amzn.to/4fn7S9f',
+    },
+    {
+      title: 'Link to Cheap Playmats',
+      href: 'https://amzn.to/4flOdXp',
     }
   ],
   freecoaching: [
